ed20dc623b814a70c4cf750c768dc19f1f9c12d0
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-04-08 00:56:32 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "6f93846f-f5c1-4e50-8737-3915574def27",
+    "home_feed": [
+        {
+            "id": "1b48ad35-6cb2-4ccd-bf77-0811f35463cc",
+            "artist_name": "fund",
+            "home_carousel": [
+                {
+                    "id": "ad832590-ba71-458f-abce-5442fa009006",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/355.png"
+                },
+                {
+                    "id": "9e99eb9a-b084-447f-866e-103d919b32b7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/122.png"
+                },
+                {
+                    "id": "cafa9dc9-51bd-43c7-8129-4b7a45fbf394",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/22.png"
+                },
+                {
+                    "id": "715d94a7-fa36-470e-8d8b-75fb5b948de4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/18.png"
+                },
+                {
+                    "id": "9a2c730a-40be-410f-8d0e-67a116235efe",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/239.png"
+                }
+            ],
+            "art_caption": "we",
+            "art_description": "South bill light practice example."
+        },
+        {
+            "id": "007e4c4c-a85f-4688-b58c-4b94584f91aa",
+            "artist_name": "almost",
+            "home_carousel": [
+                {
+                    "id": "08fc6981-ae15-4644-a396-189d3f0b7644",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/56.png"
+                },
+                {
+                    "id": "839b9516-39e1-49e6-80f6-4afe58f94daa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/81.png"
+                },
+                {
+                    "id": "aed67d21-d03e-4955-aced-9d8d879f7bed",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/253.png"
+                },
+                {
+                    "id": "f4ab785a-9b76-467e-80d2-461fabf6140a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/218.png"
+                },
+                {
+                    "id": "c2cb7d91-91bb-43cb-a34e-7c29be1bac57",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/286.png"
+                }
+            ],
+            "art_caption": "six",
+            "art_description": "Produce in cold why. Within forget alone beat according little above."
+        },
+        {
+            "id": "c311cb27-6643-4c00-862c-50a2332667e6",
+            "artist_name": "white",
+            "home_carousel": [
+                {
+                    "id": "1fe70a05-36e8-46a1-bf4d-865981f39738",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/195.png"
+                },
+                {
+                    "id": "21e248a2-5b04-43fa-ba46-e7733e2cff19",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/139.png"
+                },
+                {
+                    "id": "f5d390b0-51d3-424f-a0f6-0f576ba775ac",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/270.png"
+                },
+                {
+                    "id": "1fe022d0-ed70-443c-bf19-31a002012585",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/337.png"
+                },
+                {
+                    "id": "5994749e-8b81-4096-a42f-eea59c2c74b6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/344.png"
+                }
+            ],
+            "art_caption": "trip",
+            "art_description": "Tv imagine operation him young. Which outside most character water."
+        },
+        {
+            "id": "db11fd93-f3df-4d82-81ca-a327f7de8f38",
+            "artist_name": "table",
+            "home_carousel": [
+                {
+                    "id": "7c76e971-172a-4796-9694-1c3da2f6f6cf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/308.png"
+                },
+                {
+                    "id": "5b656ed6-272d-482c-8403-e9519105fd66",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/309.png"
+                },
+                {
+                    "id": "f73a7fcd-011e-4373-8853-8840f1a2629c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/127.png"
+                },
+                {
+                    "id": "54bb63a2-96a8-4a9f-be6b-9b906910ab3b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/223.png"
+                },
+                {
+                    "id": "78916c49-ecee-4329-ada7-6b217d8ccd32",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/184.png"
+                }
+            ],
+            "art_caption": "energy",
+            "art_description": "News song analysis citizen. Song best may."
+        },
+        {
+            "id": "c7261733-7e3e-4a23-b919-faeeb4e5fa52",
+            "artist_name": "another",
+            "home_carousel": [
+                {
+                    "id": "fcc293ec-47c5-49d5-81d7-0b2f93330d96",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/223.png"
+                },
+                {
+                    "id": "1b068843-f39f-45b4-b7d9-a6a8c1603887",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/258.png"
+                },
+                {
+                    "id": "5b552943-e946-440c-84a6-f9a5bb6fd931",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/88.png"
+                },
+                {
+                    "id": "64143f6b-c06a-47c4-9aa1-78917d98a4bf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/166.png"
+                },
+                {
+                    "id": "2da06b1d-8a17-4e2c-b603-2fa5a92b8514",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/294.png"
+                }
+            ],
+            "art_caption": "responsibility",
+            "art_description": "Onto scientist organization during read see. Effort what add agency available production left. Agreement every light sit body."
+        }
+    ]
+}
